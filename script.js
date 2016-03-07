3eb1c09851d83e966a93f488a1be7d6981891392
regisverdin <regisverdin@lawn-143-215-112-71.lawn.gatech.edu>
2016-03-07 02:22:37 -0500

    Sample selection per channel; UI

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ if (contextClass) {
   // Web Audio API is not available. Ask the user to use a supported browser.
   window.alert("webaudio is not available! use chrome please")
 }
+audioContext.bufferList = new Array();
 
 
 // Populate dropdown for selecting number of channels
@@ -34,75 +35,113 @@ for(var i = numChannels; i >= 0; i--) {
 }
 
 
+
+
+
 var speakerArray;   //speakerArray[i][0] is xpos, [i][1] is ypos, [i][2] is the node. i is the speaker/channel number
 
 function selectSpeakerNum() {
+    //Set channel numbers, add gain nodes
     var numSpeakers = document.getElementById("channels").value;
     audioContext.destination.channelCount = numSpeakers;
     if(numSpeakers == 0) {
         for (var i = 0; i < speakerArray.length; i++) {
             speakerArray[i][2].gain.value = 0;
         }
-
         return;
     }
-    console.log("speakers: ", numSpeakers);
 
+    //Make a merger (routes audio to diff channels)
+    audioContext.merger = audioContext.createChannelMerger(numSpeakers);
+    audioContext.merger.connect(audioContext.destination);
+
+
+    //Draw speakers
     var angleOffset = document.getElementById("slider").value;
     if(typeof speakerArray == 'undefined') speakerArray = [];
     else speakerArray.splice(0,speakerArray.length);   //clear the array without making a new one (which would mess up references)
-
     drawSpeakers(numSpeakers, angleOffset);
 
-    //Add DOM elements for sample selection (dropdown menu with id="thisSpeakerNumber", and delay inputs in ms. (need to add sampleSelect function later). 
+    //Add DOM elements for sample selection (dropdown menu with id="thisSpeakerNumber", and delay inputs in ms. 
     var spanNode = document.getElementById("sampleMenu");
 
     for (var i = 1; i <= numSpeakers; i++) {
         var newSelect = document.createElement("select");
         newSelect.setAttribute("id", "speaker" + i);
+        newSelect.setAttribute("onchange", "connectBuffer(this)");
+        newSelect.setAttribute("class", "light");
 
         var newLabel = document.createElement("label");
         newLabel.innerHTML = i + " :";
         // newElement.setAttribute("id", i);
         spanNode.appendChild(newLabel);
         spanNode.appendChild(newSelect);
+        spanNode.appendChild(document.createElement("br"));
+    
 
-        // for(var i = numSpeakers; i >= 0; i--) {
-        //     var option = document.createElement('option');
-        //     option.text = option.value = i;
-        //     newLabel.add(option, 0);
-        // }
+        for(var j = audioContext.bufferList.length-1; j >= 0; j--) {    //SET THIS DYNAMICALLY LATER... not sure how with async fileload (i < audioContext.bufferList.length)
+            var option = document.createElement('option');
+            option.text = option.value = audioContext.bufferList[j][0];
+            newSelect.add(option, 0);
+        }
+
+        var blankOption = document.createElement('option');
+        blankOption.value = -1;
+        blankOption.text = "--choose a clip--";
+        newSelect.add(blankOption, 0);
+    //Add delay fields
+
     }
-    //Add delay nodes
 
-
-
-    //MOVE STUFF BELOW HERE TO SAMPLESELECT FUNCTION
-
-    //Add or remove buffer nodes for each channel (drawSpeakers() takes care of clearing the old speakerArray?). DO I NEED TO REMOVE POINTERS TO DELETED NODES?
-    merger = audioContext.createChannelMerger(numSpeakers);
-    merger.connect(audioContext.destination);
-    for (var i = 0; i < numSpeakers; i++) {
-        var source = audioContext.createBufferSource(); //Creates an AudioBufferSourceNode
-        source.buffer = audioContext.audioBuffer; //Loads the buffer into the source node
-        gain = audioContext.createGain();
-        source.connect(gain);
-        gain.connect(merger, 0, i); // Assigns the source to a specific channel input on the destination
-        source.loop = true;
-        source.start(0);
-
-        speakerArray[i][2] = gain;
-            //Add dom elements for selecting sample for each speaker.
-        // addSampleMenu(speakerNum);
-    }
 }
+
+
+
+function connectBuffer(element) {
+    // Get speaker number
+    var speakerNum = element.id.slice(7,8) - 1; 
+    var sampleName = element.value;
+    console.log(sampleName);
+    console.log(speakerNum);
+
+    //Add&remove buffer nodes for each channel.
+    var source = audioContext.createBufferSource(); //Creates an AudioBufferSourceNode
+
+    //If already a sample on current channel: turn down gain (necessary to avoid click?) and disconnect node.
+    if(typeof speakerArray[speakerNum][2] != 'undefined') {
+        speakerArray[speakerNum][2].gain.value = 0;
+        speakerArray[speakerNum][2].disconnect();
+    }
+
+    for(var i=0; i < audioContext.bufferList.length; i++) {                                                 //REDO THIS USING OBJECTS STORED IN ARRAY for bufferlist (not nested arrays)
+        if(audioContext.bufferList[i][0] == sampleName) {    // If name matches array element...
+            source.buffer = audioContext.bufferList[i][1]; //...load the buffer into the source node.
+        }
+    }
+    // source.buffer = audioContext.bufferList[0][1]; //Loads the buffer into the source node
+    var gainNode = audioContext.createGain();
+    source.connect(gainNode);
+    gainNode.connect(audioContext.merger, 0, speakerNum); // Assigns the source to a specific channel input on the destination
+    gainNode.gain.value = 0;
+    source.loop = true;
+    source.start(0);
+
+    speakerArray[speakerNum][2] = gainNode;
+        //Add dom elements for selecting sample for each speaker.
+    // addSampleMenu(speakerNum);
+
+}
+
 
 function rotateSpeakers() {
     var numSpeakers = document.getElementById("channels").value;
     var angleOffset = document.getElementById("slider").value * -1; //flipping sign here b/c can't do in html tag
     drawSpeakers(numSpeakers, angleOffset);
-
 }
+
+
+
+
 
 
 
@@ -149,29 +188,36 @@ function drawSpeakers(numSpeaks, offset) {
 
 
 
+
 canvas.onmousemove = function assignChannelVolume(event) {
     //need to: take speakerArray. get current mouse pos. find eucl distance from each speaker to mouse pos. update gains for each node based on this
     var canvasRect = canvas.getBoundingClientRect();
     var mouseX = Math.floor((event.clientX-canvasRect.left)/(canvasRect.right-canvasRect.left)*canvas.width);   //mouse position rel to canvas
     var mouseY = Math.floor((event.clientY-canvasRect.top)/(canvasRect.bottom-canvasRect.top)*canvas.height);
-    var center = canvas.width/2;
+    // var center = canvas.width/2;
+    // var spriteSize = canvas.width / 30.0;
+
+    // canvasContext.fillStyle = "#5a5c51";
+    // canvasContext.fillRect(mouseX, mouseY, spriteSize, spriteSize);
 
     if(typeof speakerArray != 'undefined') {
         var r = (canvas.width / 2.0) - 20;
         for (var i = 0; i < speakerArray.length; i++) {
-            //get distance from current event point in canvas to current speaker in array
-            var a = mouseX - speakerArray[i][0];
-            var b = mouseY - speakerArray[i][1];
-            var c = Math.sqrt((a*a) + (b*b));   //distance from mouse to speaker
-            console.log("c = " + c);
-            console.log("r = " + r);
+            if(typeof speakerArray[i][2] != 'undefined') {
+                //get distance from current event point in canvas to current speaker in array
+                var a = mouseX - speakerArray[i][0];
+                var b = mouseY - speakerArray[i][1];
+                var c = Math.sqrt((a*a) + (b*b));   //distance from mouse to speaker
+                // console.log("c = " + c);
+                // console.log("r = " + r);
 
-            //gain as percentage of diameter
-            var gainAmt = Math.max(0, 1-(c/(1.2*r)));
-            console.log("gain = " + gainAmt);
+                //gain as percentage of radius (roughly)
+                var gainAmt = Math.max(0, 1-(c/(1.2*r)));
+                // console.log("gain = " + gainAmt);
 
-            //set gain for each channel
-            speakerArray[i][2].gain.value = gainAmt;
+                //set gain for each channel
+                speakerArray[i][2].gain.value = gainAmt;
+            }
         }
     }
 }
@@ -190,14 +236,25 @@ function loadFile(file) {
     getSound.responseType = "arraybuffer";
     getSound.onload = function() {
         audioContext.decodeAudioData(getSound.response, function(buffer){  //decodeAudioData returns an AudioBuffer object to use with AudioBufferSourceNode(same as AudioContext.createBuffer())
-            audioContext.audioBuffer = buffer;
-            // audioContext.audioBuffers
+            audioContext.bufferList.push([file, buffer]);
         });
     }
     getSound.send();
 }
 
-loadFile("sample.wav");
-loadFile("sample.wav");
-loadFile("sample.wav");
-loadFile("sample.wav");
+
+
+// loadFile("sample1.wav");
+// loadFile("sample2.wav");
+// loadFile("sample3.wav");
+// loadFile("sample4.wav");
+
+
+loadFile("round/round1.wav");
+loadFile("round/round2.wav");
+loadFile("round/round3.wav");
+loadFile("round/round4.wav");
+loadFile("round/round5.wav");
+loadFile("round/round6.wav");
+loadFile("round/round7.wav");
+
